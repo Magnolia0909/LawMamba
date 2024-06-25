7c8f7c92dea133339832035c62f2f15ac9d541bc
Magnolia0909 <88096429+Magnolia0909@users.noreply.github.com>
2024-06-25 15:21:50 +0800

    Update vocab.py

--- util/vocab.py
+++ util/vocab.py
@@ -8,19 +8,19 @@ import pandas as pd
 from collections import Counter
 all_texts = []
 
-with open("/home/sxx/experiment/data/law_data/test_json_v3.json", "r", encoding="utf-8") as f:
+with open("/data/law_data/test_json_v3.json", "r", encoding="utf-8") as f:
     data = json.load(f)
     for entry in data:
         if "contract" in entry:
             all_texts.append(entry["contract"])
 
-with open("/home/sxx/experiment/data/law_data/train_json_v3.json", "r", encoding="utf-8") as f:
+with open("/data/law_data/train_json_v3.json", "r", encoding="utf-8") as f:
     test_data = json.load(f)
     for entry in test_data:
         if "contract" in entry:
             all_texts.append(entry["contract"])
 
-law_df = pd.read_csv("/home/sxx/experiment/data/law_data/law.csv", encoding="utf-8")
+law_df = pd.read_csv("/data/law_data/law.csv", encoding="utf-8")
 all_texts.extend(law_df['law_content'])
 
 vocab = {}
@@ -31,7 +31,7 @@ end = "<END>"
 # num = "NUM"
 next_index = 0
 unique_words_set = set()
-with open("/home/sxx/experiment/data/law_data/te/vocab.txt", "w", encoding="utf-8") as vocab_file:
+with open("/data/law_data/te/vocab.txt", "w", encoding="utf-8") as vocab_file:
     vocab_file.write(f"{pad} {next_index}\n")
     next_index += 1
     vocab_file.write(f"{unk} {next_index}\n")
@@ -66,7 +66,7 @@ with open("/home/sxx/experiment/data/law_data/te/vocab.txt", "w", encoding="utf-
             next_index += 1
             vocab_file.write(f"{token} {vocab[token]}\n")
 # 打开文件并计算行数
-with open('/home/sxx/experiment/data/law_data/te/vocab.txt', "r", encoding="utf-8") as file:
+with open('/data/law_data/te/vocab.txt', "r", encoding="utf-8") as file:
     lines = file.readlines()
     line_count = len(lines)
 # 输出行数
